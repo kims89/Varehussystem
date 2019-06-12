3fc629bdfdd1cb7276733054d4933c9aa8215730
Kim Tore <kim@kims-mac-mini.lan>
2019-06-12 22:42:48 +0200

    updated port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var compression = require('compression');
 var admconfig = require('./config/adminInfo.js');
 var GetRouter = require('./config/GetRouter');
 var PostRouter = require('./config/PostRouter');
-var port = process.env.PORT || 8084;
+var port = process.env.PORT || 8081;
 
 moment.locale("nb");
 var app = express();
